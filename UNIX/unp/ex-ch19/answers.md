59b92a31f57938bc8491b0b340632dfabc4a902f
tcharding <me@tobin.cc>
2015-10-14 11:16:55 +1100

    unp: completed exercises ch 19

---
+++ UNIX/unp/ex-ch19/answers.md
@@ -0,0 +1,5 @@
+UNIX Network Programming - Chapter 19
+======================================
+Exercise answers - Tobin Harding.
+
+1. dump_sa_msgs.c
---
+++ UNIX/unp/ex-ch19/dump_sa_msgs.c
@@ -0,0 +1,57 @@
+#include "unp.h"
+
+#define BUFLEN 4096
+
+void sadb_dump(int type);	/* unp page 515 */
+
+int main(void)
+{
+	int s;
+	char buf[BUFLEN];
+
+	s = Socket(PF_KEY, SOCK_RAW, PF_KEY_V2);
+	
+	while ((msglen = read(s, &buf, sizeof(buf))) > 0){
+		msgp = (struct sadb_msg *) &buf;
+		print_sadb_msg(msgp, msglen);
+	}
+	close(s);
+}
+
+/* Authors: W. R. Stevens, B. Fenner, A. M. Rudoff */ 
+void
+sadb_dump(int type)
+{
+	int s;
+	char buf[4096];
+	struct sadb_msg msg;
+	int goteof;
+
+	s = Socket(PF_KEY, SOCK_RAW, PF_KEY_V2);
+
+	/* Build and write SADB_DUMP request */
+	bzero(&msg, sizeof(msg));
+	msg.sadb_msg_version = PF_KEY_V2;
+	msg.sadb_msg_type = SADB_DUMP;
+	msg.sadb_msg_satype = type;
+	msg.sadb_msg_len = sizeof(msg) / 8;
+	msg.sadb_msg_pid = getpid();
+	printf("Sending dump message:\n");
+	print_sadb_msg(&msg, sizeof(msg));
+	Write(s, &msg, sizeof(msg));
+
+	printf("\nMessages returned:\n");
+	/* Read and print SADB_DUMP replies until done */
+	goteof = 0;
+	while (goteof == 0) {
+		int msglen;
+		struct sadb_msg *msgp;
+
+		msglen = Read(s, &buf, sizeof(buf));
+		msgp = (struct sadb_msg *)&buf;
+		print_sadb_msg(msgp, msglen);
+		if (msgp->sadb_msg_seq == 0)
+			goteof = 1;
+	}
+	close(s);
+}
