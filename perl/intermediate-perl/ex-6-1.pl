048b376d869c8bf3ecdbd2cdf76c26fcddd69149
tcharding <me@tobin.cc>
2015-10-19 17:16:18 +1100

    ip: completed exercises ch 6

---
+++ perl/intermediate-perl/ex-6-1.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Storable;
+
+# Parse and print network usage report
+#
+# log format: from.where to.where num
+
+
+# Parse
+my %total;
+my $db = "running.dat";
+
+if (-e $db) {
+    my $data = retrieve $db;
+    %total = %$data;
+}
+
+while (<>) {
+    my ($src, $dst, $bytes) = split;
+    $total{$src}{$dst} += $bytes;
+}
+
+nstore \%total, $db;
+
+my @entries;
+for my $source (sort keys %total) {
+    my $sum;
+    my @entry;
+    push @entry, $source;
+    my %transfers;
+    for my $destination (sort keys %{ $total{$source} }) {
+	$transfers{$destination} = $total{$source}{$destination};
+	$sum += $total{$source}{$destination};
+    }
+    push @entry, $sum;
+    push @entry, \%transfers;
+    push @entries, \@entry;
+}
+
+@entries = sort { $a->[1] cmp $b->[1] } @entries;
+
+# Print
+for my $entry_ref  (@entries) {
+    my ($source, $sum, $transfers) = @{$entry_ref};
+
+    printf "%s\n", $source;
+    for (sort keys %{$transfers}) {
+	printf "\t%-25s %15s\n", $_, ${$transfers}{$_};
+    }
+    print "\n";
+    print "\n";
+}
