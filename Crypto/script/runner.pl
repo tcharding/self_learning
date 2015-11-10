2be54d02060e9c57bbf3c7680dd7f684beec6369
tcharding <me@tobin.cc>
2015-11-10 16:49:10 +1100

    re-factored chal 13

--- Crypto/script/runner.pl
+++ Crypto/script/runner.pl
@@ -4,7 +4,7 @@ use warnings;
 use feature qw/say/;
 
 my @set1 = qw( chal-1.pl chal-2.pl chal-3.pl chal-5.pl skip-6 skip-7 chal-8.pl);
-my @set2 = qw( chal-9.pl chal-10.pl skip-11 skip-12);
+my @set2 = qw( chal-9.pl chal-10.pl skip-11 skip-12 skip-13);
 
 run( "Set 1", @set1 );
 run( "Set 2", @set2 );
