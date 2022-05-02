151ba946ed4c3777461a207e694f826d3968b54f
Tobin C. Harding <me@tobin.cc>
2022-05-02 18:01:23 +1000

    Add remaining programs for chapter 1

--- aic/ch1/src/main.rs
+++ aic/ch1/src/main.rs
@@ -2,12 +2,12 @@ use anyhow::{bail, Result};
 use std::io;
 
 fn main() -> Result<()> {
-    quick_find()?;
+    path_compression()?;
     Ok(())
 }
 
 /// Arbitrary maximum value for p and q.
-const N: usize = 10;
+const N: usize = 10; // This is 10000 in the text.
 
 /// Program 1.1: Quick-find solution to connectivity problem.
 fn quick_find() -> Result<()> {
@@ -29,6 +29,122 @@ fn quick_find() -> Result<()> {
                     if id[i] == t {
                         id[i] = id[q];
                     }
+                }
+                println!("{} {}", p, q);
+            }
+        }
+    }
+}
+
+/// Program 1.2: Quick-union solution to connectivity problem.
+fn quick_union() -> Result<()> {
+    let mut id: Vec<usize> = Vec::with_capacity(N);
+
+    for i in 0..N {
+        id.push(i);
+    }
+
+    loop {
+        match read_input_values()? {
+            None => return Ok(()),
+            Some((p, q)) => {
+                let mut i = p;
+                while i != id[i] {
+                    i = id[i]
+                }
+
+                let mut j = q;
+                while j != id[j] {
+                    j = id[j]
+                }
+
+                if i == j {
+                    continue;
+                }
+                id[i] = j;
+                println!("{} {}", p, q);
+            }
+        }
+    }
+}
+
+/// Program 1.3: Weighed version of quick union.
+fn weighted_quick_union() -> Result<()> {
+    let mut id: Vec<usize> = Vec::with_capacity(N);
+    let mut sz: Vec<usize> = Vec::with_capacity(N);
+
+    for i in 0..N {
+        id.push(i);
+        sz.push(1);
+    }
+
+    loop {
+        match read_input_values()? {
+            None => return Ok(()),
+            Some((p, q)) => {
+                let mut i = p;
+                while i != id[i] {
+                    i = id[i]
+                }
+
+                let mut j = q;
+                while j != id[j] {
+                    j = id[j]
+                }
+
+                if i == j {
+                    continue;
+                }
+
+                if sz[i] < sz[j] {
+                    id[i] = j;
+                    sz[j] += sz[i];
+                } else {
+                    id[j] = i;
+                    sz[i] += sz[j];
+                }
+                println!("{} {}", p, q);
+            }
+        }
+    }
+}
+
+/// Program 1.4: Path compression by halving
+fn path_compression() -> Result<()> {
+    let mut id: Vec<usize> = Vec::with_capacity(N);
+    let mut sz: Vec<usize> = Vec::with_capacity(N);
+
+    for i in 0..N {
+        id.push(i);
+        sz.push(1);
+    }
+
+    loop {
+        match read_input_values()? {
+            None => return Ok(()),
+            Some((p, q)) => {
+                let mut i = p;
+                while i != id[i] {
+                    id[i] = id[id[i]];
+                    i = id[i];
+                }
+
+                let mut j = q;
+                while j != id[j] {
+                    id[j] = id[id[j]];
+                    j = id[j]
+                }
+
+                if i == j {
+                    continue;
+                }
+
+                if sz[i] < sz[j] {
+                    id[i] = j;
+                    sz[j] += sz[i];
+                } else {
+                    id[j] = i;
+                    sz[i] += sz[j];
                 }
                 println!("{} {}", p, q);
             }
